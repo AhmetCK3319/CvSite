080d9afbbabdba4af93533cd15ee04ed4a82f84a
ahmet can koca <ahmetcankoca1990@hotmail.com>
2023-12-13 01:53:46 +0300

    feat: docker comp

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.10-slim
 RUN apt-get update
 RUN apt-get install python3-dev build-essential -y
 
+ENV PYTHONDONTWRITEBYTECODE 1
+
 RUN pip install --upgrade pip
 RUN pip install virtualenv && python -m virtualenv /opt/venv
 
--- resume/settings.py
+++ resume/settings.py
@@ -9,18 +9,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
 import environ
 
 env=environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -105,8 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -117,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
