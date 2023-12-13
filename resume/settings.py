cb7f8356202dcc92776c9c5121c7b8c674aed1f1
ahmet can koca <ahmetcankoca1990@hotmail.com>
2023-12-13 21:33:48 +0300

    feat: core model

--- resume/settings.py
+++ resume/settings.py
@@ -117,7 +117,24 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alican331957@gmail.com'
+EMAIL_HOST_PASSWORD = 'oqtm qjcn ixqq skyd'
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
